3fe8f06e237efee23ef92fd85d92bcc2137c281c
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-01-07 21:44:54 +0600

    added delete

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ const dbConnect = async () => {
       }
   })
 
+  // delete 
+app.delete('/my/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = { _id : new ObjectId(id)}
+  const result = await haiku.deleteOne(query)
+  res.send(result)
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(

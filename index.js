2b2054a0c44937c833c0532dace13421be5457db
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-01-13 22:58:21 +0600

    user added api

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = 3000;
 require("dotenv").config();
 // app.use(cors({}));
 app.use(express.json());
-app.use(cors({
-  origin: [
-    'http://localhost:5173'   
-],
-  credentials: true 
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
-
-app.use(cors({origin:true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 const { MongoClient, ServerApiVersion } = require("mongodb");
 // mongo
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.kkqbu90.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -36,62 +35,68 @@ const dbConnect = async () => {
     const database = client.db("SiddikShope");
     const userInfoCollection = database.collection("users");
     const productInfoCollection = database.collection("products");
+    // post user insert
+    app.post("/users", async (req, res) => {
+      try {
+        const body = req.body;
+        // have any user 
+        const query = {email:body?.email}
+        const existingUser = await userInfoCollection.findOne(query)
+        if (existingUser) {
+          return res.send({massage:"User already exist"})
+        }
+        const result = await haiku.insertOne(body);
+        res.send(result);
+        // console.log(body);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 
-
-    // jwt 
-    app.post("/authentication",async(req,res) => {
-   const userEmail = req.body;
-   console.log(userEmail)
-   const token =jwt.sign(userEmail,process.env.ACCESS_KEY_TOKEN,{expiresIn:"10d"})
-   res.send({token})
-    })
+    // jwt
+    app.post("/authentication", async (req, res) => {
+      const userEmail = req.body;
+      console.log(userEmail);
+      const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, {
+        expiresIn: "10d",
+      });
+      res.send({ token });
+    });
     // get
-
 
     app.get("/add", async (req, res) => {
       const alladd = haiku.find();
       const result = await alladd.toArray();
       res.send(result);
     });
-     // post ass
-     app.post('/ass',async(req,res)=>{
-      try{
-     const body = req.body
-     const result = await haiku.insertOne(body)
-     res.send(result)
-     console.log(body)
-      }catch(err){
-          console.log(err)
-      }
-  })
 
-  // delete 
-app.delete('/my/:id',async(req,res)=>{
-  const id = req.params.id;
-  const query = { _id : new ObjectId(id)}
-  const result = await haiku.deleteOne(query)
-  res.send(result)
-})
-// update data form submited data 
-// app.put('/submits/:id',async(req,res)=>{
-//   const id = req.params.id
-//   const filter = {_id :new ObjectId(id)}
-//   const options ={ upset:true}
-// const updateDs= req.body 
-// const updateds ={
-//   $set:{
-    
-//     mark:updateDs.mark,
-//     note:updateDs.note,
-//     stutas:updateDs.stutas,
-//     textarea:updateDs.textarea 
-   
-//   }
+    // delete
+    app.delete("/my/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await haiku.deleteOne(query);
+      res.send(result);
+    });
+    // update data form submited data
+    // app.put('/submits/:id',async(req,res)=>{
+    //   const id = req.params.id
+    //   const filter = {_id :new ObjectId(id)}
+    //   const options ={ upset:true}
+    // const updateDs= req.body
+    // const updateds ={
+    //   $set:{
 
-// }
-// const result = await submitedData.updateOne(filter,updateds,options)
-// res.send(result)
-// })
+    //     mark:updateDs.mark,
+    //     note:updateDs.note,
+    //     stutas:updateDs.stutas,
+    //     textarea:updateDs.textarea
+
+    //   }
+
+    // }
+    // const result = await submitedData.updateOne(filter,updateds,options)
+    // res.send(result)
+    // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
